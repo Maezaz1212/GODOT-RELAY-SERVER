e6280a139ea70823f21486c046b102a3a47bae37
Shazeel Ali <75409190+ShazeelAli@users.noreply.github.com>
2024-07-26 20:43:42 +1200

    UPDATING

--- RELAY.gd
+++ RELAY.gd
@@ -1,41 +1,46 @@
-extends Node2D
+extends Node
 
-
+const ROOM_CODE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 @export var PLAYER_DICT = {}
 @export var ROOMS = {}
-var CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+
+### ALL CODE PERTAINING TO RELAY CONNECTION
 
 ## Supplementary functions
 func create_room_code():
 	var id = ""
 	for n in 5:
-		var random_number =  randi_range(0,CHARS.length() -1)
-		var random_char = CHARS[random_number]
+		var random_number =  randi_range(0,ROOM_CODE_CHARS.length() -1)
+		var random_char = ROOM_CODE_CHARS[random_number]
 		id+=random_char
 	if ROOMS.has(id):
 		return create_room_code()
 	
 	return id
 	
-### ALL CODE PERTAINING TO RELAY CONNECTION
-# Called when the node enters the scene tree for the first time
-# Sets Up Server Socket
+
 func _ready():
-	var relay_peer = ENetMultiplayerPeer.new()
-	var error = relay_peer.create_server(25566)
+	
+	var relay_peer = WebSocketMultiplayerPeer.new()
+	var crypto = Crypto.new()
+	var key = crypto.generate_rsa(4096)
+	var cert = crypto.generate_self_signed_certificate(key, "CN=localhost,O=myorganisation,C=IT")
+	var error = relay_peer.create_server(25566,"*",TLSOptions.server(key,cert))
+	
 	if error:
 		return(error)
 	multiplayer.multiplayer_peer = relay_peer
-	
 	multiplayer.peer_disconnected.connect(_on_player_disconnected)
+	
 	
 ## CREATES PLAYER ON JOIN
 @rpc("any_peer","call_remote","reliable")
-func _resgister_player():
+func _resgister_player(game_name):
 	var new_player_id = multiplayer.get_remote_sender_id()
 	PLAYER_DICT[new_player_id] = {
-		"player_name":"EMPTY",
 		"room_code":"EMPTY",
+		"game_name":game_name,
 		"is_host":false,
 		"multiplayer_id":0
 	}
@@ -47,13 +52,14 @@ func _on_player_disconnected(id):
 
 	var player_data = PLAYER_DICT[id]
 	var room_code = player_data.room_code
-	PLAYER_DICT.erase(id)
+	
 	if player_data.is_host == true:
 		ROOMS[room_code].players.erase(id)
-		close_room(room_code)
+		close_room(room_code,id)
 	elif ROOMS.has(room_code):
 		ROOMS[room_code].players.erase(id)
 		remove_player(room_code,id)
+	PLAYER_DICT.erase(id)
 
 @rpc("any_peer","call_remote","reliable")
 func leave_command():
@@ -61,13 +67,15 @@ func leave_command():
 	var player_room_id = PLAYER_DICT[sender_id].room_code
 	if ROOMS.has(player_room_id):
 		if ROOMS[player_room_id].host_id == sender_id:
-			close_room(player_room_id)
+			close_room(player_room_id,sender_id)
 		else:
 			remove_player(player_room_id,sender_id)
 
-func close_room(room_code):
+func close_room(room_code,sender_id):
 	for player_id in ROOMS[room_code].players:
 		room_closed.rpc_id(player_id)
+	PLAYER_DICT[sender_id].is_host = false
+	PLAYER_DICT[sender_id].room_code = "EMPTY"
 	ROOMS.erase(room_code)
 	
 @rpc("any_peer","call_remote","reliable")
@@ -97,14 +105,14 @@ func host_rpc():
 	PLAYER_DICT[sender_id].is_host = true;
 	ROOMS[room_code] = {
 		"room_code":room_code,
-		"host_id":0,
+		"game_name":PLAYER_DICT[sender_id].game_name,
+		"host_id":sender_id,
 		"players":{},
 		"max_players":3,
 		"game_started":false,
 		"is_public":false
 	}
 	
-	ROOMS[room_code].host_id = sender_id
 	ROOMS[room_code].players[sender_id] = PLAYER_DICT[sender_id]
 	host_success_rpc.rpc_id(sender_id,room_code,ROOMS[room_code])
 	sync_room_data_all(room_code)
@@ -134,6 +142,10 @@ func join_rpc(room_code : String):
 		join_fail_rpc.rpc_id(sender_id,room_code,"GAME HAS STARTED")
 		return
 	
+	if ROOMS[room_code].game_name != PLAYER_DICT[sender_id].game_name:
+		join_fail_rpc.rpc_id(sender_id,room_code,"CODE NOT FOR THIS GAME")
+		return
+		
 	PLAYER_DICT[sender_id].room_code = room_code
 	ROOMS[room_code].players[sender_id] = PLAYER_DICT[sender_id]
 	for player_id in ROOMS[room_code].players:
